6a48a72839b07b99a186ca86ff278896d34223bc
David Alkire <dalkire@wayfair.com>
2019-03-24 17:19:53 -0400

    Initial Commit

---
+++ LiveChessViewer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  LiveChessViewer
+//
+//  Created by David Alkire on 3/24/19.
+//  Copyright © 2019 David Alkire. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
